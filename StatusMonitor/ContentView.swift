c5463079a696122dea62b09bdc639882321e98b9
Zaahir Moolla <moollaza@duckduckgo.com>
2026-04-09 17:05:05 -0400

    Initial Commit

---
+++ StatusMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StatusMonitor
+//
+//  Created by Zaahir Moolla on 2026-04-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StatusMonitor/StatusMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusMonitorApp.swift
+//  StatusMonitor
+//
+//  Created by Zaahir Moolla on 2026-04-09.
+//
+
+import SwiftUI
+
+@main
+struct StatusMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
